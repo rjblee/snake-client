6fe0c0fc1ae4b259a6c79c475d3c1aee8b3311de
Ralph <ralph.lee@live.ca>
2019-07-03 21:42:04 +0000

    Added Name: inital

--- client.js
+++ client.js
@@ -1,34 +1,24 @@
 const net = require('net');
-const conn = net.createConnection({ 
-  host: '192.168.88.126', // change to IP address
-  port: 3000
-});
-
-conn.setEncoding('utf8'); // interpret data as text
-
-// client.js
-conn.on('data', (data) => {
-  console.log('Server says: ', data);
-});
-
-conn.on('connect', () => {
-  conn.write('Hi Julia!');
-});
 
 const connect = function() {
-    const conn = net.createConnection({ 
-      host: '192.168.88.218',
-      port: 50541
-    });
+  const conn = net.createConnection({ 
+    host: '192.168.88.218',
+    port: 50541
+  });
     // interpret incoming data as text
-    conn.setEncoding('utf8'); 
+  conn.setEncoding('utf8'); 
     
     // attached an event handler to handle incoming data and console log it for the player.
-    conn.on('data', function(message) {
-      console.log(message);
-    });
+  // conn.on('data', function(message) {
+  //   console.log(message);
+  // });
+
+  conn.on('connect', function() {
+    conn.write("Name: R.L");
+    console.log("Successfully connected to game server");
+  });
   
-    return conn;
-  };
+  return conn;
+};
 
 module.exports = connect;
