5446fd64cfeb52d01cf3b4e4e39f28d272e49345
Ralph <ralph.lee@live.ca>
2019-07-03 20:21:31 +0000

    Added snake event handler

---
+++ play.js
@@ -0,0 +1,23 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '192.168.88.218',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+  
+  // attached an event handler to handle incoming data and console log it for the player.
+  conn.on('data', function(message) {
+    console.log(message);
+  });
+
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
