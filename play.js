bcdbf16693ab2f52101a2f31011e96466c49f2d3
Ralph <ralph.lee@live.ca>
2019-07-03 20:55:36 +0000

    Added connection module

--- play.js
+++ play.js
@@ -1,23 +1,24 @@
-const net = require('net');
+// const net = require('net');
 
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '192.168.88.218',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+// const connect = function() {
+//   const conn = net.createConnection({ 
+//     host: '192.168.88.218',
+//     port: 50541
+//   });
+//   // interpret incoming data as text
+//   conn.setEncoding('utf8'); 
   
-  // attached an event handler to handle incoming data and console log it for the player.
-  conn.on('data', function(message) {
-    console.log(message);
-  });
+//   // attached an event handler to handle incoming data and console log it for the player.
+//   conn.on('data', function(message) {
+//     console.log(message);
+//   });
 
-  return conn;
-}
+//   return conn;
+// }
 
+const connect = require('./client');
 console.log('Connecting ...');
 connect();
