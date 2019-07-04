4637ce1d0113ef8a9ca6f28feb6449ddb82664e3
Ralph <ralph.lee@live.ca>
2019-07-04 00:22:12 +0000

    Added the ctrl+C to end game

--- play.js
+++ play.js
@@ -22,3 +22,16 @@
 const connect = require('./client');
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+    }
+  })
+  return stdin;
+}
